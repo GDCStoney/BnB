b78a933fbb00836a1eb2a26d4a8df3e0b819cacb
Bhupesh <bhupesh100@gmail.com>
2020-11-09 16:37:08 +0000

    Added database functionality

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -1,6 +1,19 @@
 require 'pg'
 
 class DatabaseConnection
-  
+
+  @database
+
+  def self.setup(db_name)
+    @database = PG.connect(dbname: db_name)
+  end
+
+  def self.query(query)
+    @database.exec(query)
+  end
+
+  def self.database
+    @database
+  end
 
 end
