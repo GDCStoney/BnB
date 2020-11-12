912798c5f397b9ebfeec5a7771d1d3bd9b6a7c35
GDCStoney <g.stoney@btinternet.com>
2020-11-12 23:06:36 +0000

    initial piece for managing images

--- app.rb
+++ app.rb
@@ -62,7 +62,17 @@ class BnB < Sinatra::Base
   end
 
   post '/listing/add' do
+    if params[:image] && params[:image][:filename]
+      filename = params[:image][:filename]
+      file = params[:image][:tempfile]
+      path = "./public/uploads/#{filename}"
 
+      File.open(path, 'wb+') do |f|
+        f.write(file.read)
+      end
+    end
+    result = Listing.create(name: params[:name], description: params[:description], host_id: session[:user].id, price: params[:price], start_date: params[:start_date], end_date: params[:end_date], image_filename: path)
+    redirect '/'
   end
 
   get '/listing/edit' do
@@ -72,7 +82,7 @@ class BnB < Sinatra::Base
 
   post '/listing/edit' do
     description_for_query = params[:description].gsub("'", "''")
-    @listing = Listing.update(name: params[:name], description: description_for_query, price: params[:price], start_date: params[:start_date], end_date: params[:end_date], id: params[:id])
+    @listing = Listing.update(name: params[:name], description: description_for_query, price: params[:price], start_date: params[:start_date], end_date: params[:end_date], id: params[:id], image_filename: params[image_filename])
     redirect '/'
   end
 
